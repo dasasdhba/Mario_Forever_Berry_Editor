332e8eac41e4d95e3ccff65ae18557a701da3edb
dasasdhba <dasasdhba@outlook.com>
2022-07-10 20:40:42 +0800

    pause adjust

--- system/singleton/script/pause/pause_label.gd
+++ system/singleton/script/pause/pause_label.gd
@@ -6,10 +6,13 @@ func _ready() ->void:
 	pause_key_update()
 	
 func pause_key_update() ->void:
-	var event :InputEvent
+	var event :InputEvent = null
 	for i in InputMap.get_action_list("ui_pause"):
 		if i is InputEvent:
 			event = i
 			break
+	if event == null:
+		text = ""
+		return
 	var pause_key :String = OS.get_scancode_string(event.get_scancode_with_modifiers())
 	text = origin_text.replace("%s",pause_key)
