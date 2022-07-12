498e30f55c4c4e4529f493e6cf3b24c6fda46eb7
dasasdhba <dasasdhba@outlook.com>
2022-07-12 11:25:23 +0800

    lakitu bug fix

--- script/tool_script/draw_horizontal_border.gd
+++ script/tool_script/draw_horizontal_border.gd
@@ -1,9 +1,8 @@
 tool
 extends Node2D
 
-export var start :float = -480
-export var height :float = 10016
 export var preview_color :Color = Color(0.5,0.5,0,0.8)
+export var preview_height :int = 1080
 export var width :float = 2
 
 func _ready() ->void:
@@ -14,8 +13,10 @@ func _draw() ->void:
 	var parent :Node = get_parent()
 	var xleft :float = parent.left_border - parent.global_position.x
 	var xright :float = parent.right_border - parent.global_position.x
-	var ytop :float = start - parent.global_position.y
-	var ybottom :float = height - parent.global_position.y
+	var pos :Vector2 = get_global_mouse_position() - global_position
+	var y :float = floor(pos.y/32)*32 - preview_height
+	var ytop :float = y
+	var ybottom :float = y + 2*preview_height
 	var trans :Transform2D = parent.global_transform.affine_inverse()
 	trans.origin = Vector2.ZERO
 	draw_set_transform_matrix(trans)
