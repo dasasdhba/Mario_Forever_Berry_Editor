5b354731fc26c9e5333e26a8fd70750ebcef071a
dasasdhba <dasasdhba@outlook.com>
2022-07-16 11:54:45 +0800

    stomp defer fix

--- script/object_script/player/player_stomp_enemy.gd
+++ script/object_script/player/player_stomp_enemy.gd
@@ -3,7 +3,7 @@ extends RectCollision2D
 onready var parent :Node = get_parent()
 
 func _physics_process(_delta):
-	if parent.star:
+	if parent.star || parent.disable_deferred || parent.get_parent() == Player || parent.get_parent() == null:
 		return
 	for i in get_overlapping_rect("enemy_stomp"):
 		i.stomp_detect(self)
