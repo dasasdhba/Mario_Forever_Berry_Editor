b70b61ac4e22c8e4eee1000413a8a11d413c9acb
dasasdhba <dasasdhba@outlook.com>
2022-10-28 12:06:44 +0800

    block hidden tool

--- script/object_script/bonus/block/block_hidden_rect.gd
+++
@@ -1,9 +0,0 @@
-# 用于顶砖判定
-extends RectCollision2D
-
-func _ready() ->void:
-	collision_name = "block"
-	var box :RectBox2D = RectBox2D.new()
-	box.load_collision_shape(get_parent().get_node("CollisionShape2D"))
-	add_child(box)
-	rect_init()
