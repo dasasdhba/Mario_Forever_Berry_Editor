d26e8f1d46aa6a777a884c396f549faedd2f3d6b
dasasdhba <dasasdhba@outlook.com>
2022-11-10 23:21:40 +0800

    room2d _init removed

--- script/class/room2d.gd
+++ script/class/room2d.gd
@@ -9,9 +9,6 @@ export var random_seed :String = ""
 var manager :Node = Scene
 var hud :Control = null
 var node_array :Array = [] # 用于暂存部分节点
-
-func _init() ->void:
-	node_array.clear()
 	
 func _ready() ->void:
 	room2d_ready()
