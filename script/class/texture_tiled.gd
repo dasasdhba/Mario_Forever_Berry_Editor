0474fb5ee519de25bdd1a882e3b7354273cec9b7
dasasdhba <dasasdhba@outlook.com>
2022-11-20 15:43:07 +0800

    TextureTiled Spacing

--- script/class/texture_tiled.gd
+++ script/class/texture_tiled.gd
@@ -5,33 +5,45 @@ class_name TextureTiled, "icon/texture_tiled.png"
 
 export var texture :Texture = null
 export var texture_scale :Vector2 = Vector2.ONE
+enum SPACING {PIXEL, TEXTURE}
+export(SPACING) var spacing_mode :int = SPACING.PIXEL
+export var spacing :Vector2 = Vector2.ZERO
 export var expand_h :bool = false # 若 Camera 的 scale 不为 1 或者在变化，则需要启用 auto_update
 export var expand_v :bool = false
 export var velocity :Vector2 = Vector2.ZERO
 export var auto_update :bool = false
 
 var draw_size :Vector2
+var draw_gap :Vector2
 var global_draw_size :Vector2
 var global_tex_size :Vector2
 var view :Node
 
+func is_invalid() ->bool:
+	return texture == null || scale.x == 0 || scale.y == 0 || texture_scale.x == 0 || texture_scale.y == 0
+
 func update_draw_size() ->void:
-	if texture == null || texture_scale.x == 0 || texture_scale.y == 0:
+	if is_invalid():
 		draw_size = Vector2.ZERO
+		draw_gap = Vector2.ZERO
 		global_draw_size = Vector2.ZERO
 		global_tex_size = Vector2.ZERO
 		return
 	
 	var size :Vector2 = texture.get_size()
+	draw_gap = spacing.abs()
+	if spacing_mode == SPACING.TEXTURE:
+		draw_gap.x *= size.x
+		draw_gap.y *= size.y
 	if !Engine.editor_hint && (expand_h || expand_v):
-		var tex_size :Vector2 = Vector2(size.x*abs(texture_scale.x),size.y*abs(texture_scale.y))
+		var tex_size :Vector2 = Vector2(size.x*abs(texture_scale.x),size.y*abs(texture_scale.y)) + draw_gap
 		global_tex_size = Berry.get_global_position(self,tex_size).abs()
 		
 		var border_size :Vector2 = view.current_border.size
-		draw_size.x = tex_size.x * max(3,ceil(border_size.x/tex_size.x)+3) if expand_h else abs(scale.x)*size.x
-		draw_size.y = tex_size.y * max(3,ceil(border_size.y/tex_size.y)+3) if expand_v else abs(scale.y)*size.y
+		draw_size.x = tex_size.x * max(3,ceil(border_size.x/tex_size.x)+3) if expand_h else abs(scale.x)*(size.x+draw_gap.x)
+		draw_size.y = tex_size.y * max(3,ceil(border_size.y/tex_size.y)+3) if expand_v else abs(scale.y)*(size.y+draw_gap.y)
 	else:
-		draw_size = Vector2(abs(scale.x)*size.x,abs(scale.y)*size.y)
+		draw_size = Vector2(abs(scale.x)*(size.x+draw_gap.x),abs(scale.y)*(size.y+draw_gap.y))
 	
 	if !Engine.editor_hint:
 		global_draw_size = Berry.get_global_position(self,draw_size)
@@ -44,13 +56,33 @@ func _ready() ->void:
 	update_draw_size()
 
 func _draw() ->void:
-	if texture == null || scale.x == 0 || scale.y == 0:
+	if is_invalid():
 		return
 	var draw_rect :Rect2 = Rect2(Vector2.ZERO,draw_size)
 	draw_rect.end.x *= sign(texture_scale.x)/abs(texture_scale.x)
 	draw_rect.end.y *= sign(texture_scale.y)/abs(texture_scale.y)
 	draw_set_transform(Vector2.ZERO,0,Vector2(abs(texture_scale.x)/scale.x,abs(texture_scale.y)/scale.y))
-	draw_texture_rect(texture,draw_rect,true)
+	if draw_gap == Vector2.ZERO:
+		draw_texture_rect(texture,draw_rect,true)
+	else:
+		var gap :Vector2 = draw_gap
+		if spacing_mode == SPACING.PIXEL:
+			gap.x /= abs(texture_scale.x)
+			gap.y /= abs(texture_scale.y)
+		var rect_size :Vector2 = texture.get_size()
+		var size :Vector2 = rect_size + gap
+		rect_size.x *= sign(draw_rect.end.x)
+		rect_size.y *= sign(draw_rect.end.y)
+		var i :int = 0
+		while i * size.x < abs(draw_rect.end.x):
+			var j :int = 0
+			while j * size.y < abs(draw_rect.end.y):
+				var pos :Vector2 = Vector2(i * size.x, j * size.y)
+				var r :Vector2 = rect_size
+				var rect :Rect2 = Rect2(pos, r)
+				draw_texture_rect(texture,rect,true)
+				j += 1
+			i += 1
 
 func _process(_delta) ->void:
 	if Engine.editor_hint:
